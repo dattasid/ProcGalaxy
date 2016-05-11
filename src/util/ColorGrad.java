91766b08abd728d8d3edb1ea635fd63ce13c3093
sdatta <sdatta@example.com>
2016-05-11 10:03:39 -0700

    Storm

--- src/util/ColorGrad.java
+++ src/util/ColorGrad.java
@@ -2,5 +2,16 @@ package util;
 
 public class ColorGrad
 {
+    float[][] colorsHSB;
+    
+    float[] points;
 
+    public ColorGrad(float[][] colorsHSB, float[] points)
+    {
+        super();
+        this.colorsHSB = System.colorsHSB;
+        this.points = points;
+    }
+    
+    
 }
